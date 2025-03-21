1367c467bdea58cd0305c516ba7d7b94097e4abf
mudoker <huuquoc7603@gmail.com>
2025-03-21 21:33:48 +0700

    feat: setup redux, redux-toolkit and template

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,15 @@ import ReactDOM from 'react-dom/client';
 
 import '@/styles/index.css';
 
+import store from './store/store.ts';
+import { Provider } from 'react-redux'
+
 import App from './app.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <App />
+    </Provider>
   </React.StrictMode>
 );
